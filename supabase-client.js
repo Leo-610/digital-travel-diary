eb41d05867a98a33845f1b79464867af54ca5eb5
Leo-610 <763860802@qq.com>
2025-08-02 22:32:42 +0800

    修复Supabase客户端初始化重定向URL

--- supabase-client.js
+++ supabase-client.js
@@ -21,7 +21,7 @@ class SupabaseClient {
                         autoRefreshToken: true,
                         persistSession: true,
                         detectSessionInUrl: true,
-                        redirectTo: window.location.href.split('?')[0].split('#')[0],
+                        redirectTo: 'https://leo-610.github.io/digital-travel-diary',
                         flowType: 'implicit'
                     },
                     global: {
